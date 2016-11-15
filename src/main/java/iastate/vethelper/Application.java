ade9ba3029fa21c08595e4be323907a223d90666
iaharris <iaharris@iastate.edu>
2016-11-14 22:18:18 -0600

    Master - Renamed the test package - Edited the Application.java, it is now using the simplest configuration possible at the moment - Edited the mapping of the home page controller, hitting localhost:8080/home/page now prints hello-world

--- src/main/java/iastate/vethelper/Application.java
+++ src/main/java/iastate/vethelper/Application.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,19 +15,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * This is the main Spring Boot application class. It configures Spring Boot, JPA, Swagger
  */
 
-@EnableAutoConfiguration  // Sprint Boot Auto Configuration
+@SpringBootApplication  // Sprint Boot Auto Configuration
 public class Application extends SpringBootServletInitializer {
 
-    private static final Class<Application> applicationClass = Application.class;
-    private static final Logger log = LoggerFactory.getLogger(applicationClass);
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(applicationClass, args);
+		SpringApplication.run(Application.class, args);
 	}
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(applicationClass);
-    }
 
 }
